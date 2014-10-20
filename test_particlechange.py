ac7d4adaa145bd939d4bcae6c4d2a07c06c277f8
thomasgalley <thomas.galley.14@ucl.ac.uk>
2014-10-20 18:28:32 +0100

    test for empty array added

---
+++ test_particlechange.py
@@ -0,0 +1,10 @@
+from nose.tools import assert_equal 
+from diffusion_model import energy
+from particlechange import moveparticle 
+
+#test that move particle on empty array returns an empty array
+
+def test_empty_particle():
+    densities = [ [0], [0,0]]
+    for density in densities: assert moveparticle(density) == density
+    
