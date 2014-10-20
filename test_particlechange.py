17d19d658098d98f2d1ae132af3759f50af3a235
thomasgalley <thomas.galley.14@ucl.ac.uk>
2014-10-20 18:29:27 +0100

    Expanded test to include 3 component empty array

--- test_particlechange.py
+++ test_particlechange.py
@@ -5,6 +5,6 @@ from particlechange import moveparticle
 #test that move particle on empty array returns an empty array
 
 def test_empty_particle():
-    densities = [ [0], [0,0]]
+    densities = [ [0], [0,0],[0,0,0]]
     for density in densities: assert moveparticle(density) == density
     
