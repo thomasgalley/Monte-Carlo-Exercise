7ecdb82c8ccef8a8f67f109f18549f8079f69620
thomasgalley <thomas.galley.14@ucl.ac.uk>
2014-10-21 18:30:03 +0100

    Comments added to function for clarity

--- test_Energycomparison.py
+++ test_Energycomparison.py
@@ -9,23 +9,26 @@ from numpy import absolute
 
 def test_P_lies_in_correct_interval():
     
-      # From the given example have looped over vectors of differing sizes
+ # From the given example have looped over vectors of differing sizes
       
   for vector_size in randint(1, 1000, size=30): 
 
-    # Create random density of size N (code used from given example)
+ # Create random density of size N (code used from given example)
     density = randint(50, size=vector_size) 
     passdensity = moveparticle(density)
     T=randint(1,100) 
     E1 = energy(density,coefficient=1)
     E2 = energy(passdensity,coefficient=1)
     E3 = E2-E1
+    
+#test that P lies in the interval [0,1]
+
     if E3 >= 0:
         P=exp(-divide(E3,T))
     assert (P>= 0 and 1>=P)
     
 def test_only_one_particle_moves():
-#This tests whether the code can move two particle in a single step
+#This tests whether the code can move two particle in a single step (it should only move one)
 
     for vector_size in randint(1, 1000, size=30): 
 
