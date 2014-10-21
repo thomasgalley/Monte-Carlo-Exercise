1cd077690f54fdce0b12d3a35587360247d32954
thomasgalley <thomas.galley.14@ucl.ac.uk>
2014-10-21 17:59:12 +0100

    Created a test to ensure that only one particle at a time is moved

--- test_Energycomparison.py
+++ test_Energycomparison.py
@@ -5,6 +5,7 @@ from diffusion_model import energy
 from numpy.random import randint 
 from particlechange import moveparticle
 from numpy import exp, divide
+from numpy import absolute
 
 def test_P_lies_in_correct_interval():
     
@@ -22,3 +23,16 @@ def test_P_lies_in_correct_interval():
     if E3 >= 0:
         P=exp(-divide(E3,T))
     assert (P>= 0 and 1>=P)
+    
+def test_only_one_particle_moves():
+#This tests whether the code can move two particle in a single step
+
+    for vector_size in randint(1, 1000, size=30): 
+
+# Create random density of size N (code used from given example)
+        density = randint(50, size=vector_size) 
+        T=randint(1,100)
+#check that for every entry the difference between the density and the density after the comparison (and potential move) is either one or zero
+        
+        for i in (0,vector_size-1):
+            assert absolute(compare(density,T)[i]-density[i])<=1
